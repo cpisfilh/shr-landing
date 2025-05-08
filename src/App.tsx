9a0b2cfc62b8e2dc7643ec9d6fb38a9d0d31d39b
kremin12 <cdph12vilotex@gmail.com>
2025-05-08 02:01:32 -0500

    seccion "nosotros" y título en App

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ function App() {
       <Navbar />
       <Banner />
       <UsSection />
+      <div className='bg-white text-[#226752] flex flex-col md:flex-row items-center justify-center px-2 pt-[60px] md:px-20'>
+        <h1 className='text-5xl text-center font-bold'>... y este es nuestro proyecto principal</h1>
+      </div>
+
       <InfoProyecto />
       <ImagesGrid />
     </>
--- src/components/nosotros/UsSection.tsx
+++ src/components/nosotros/UsSection.tsx
@@ -2,21 +2,21 @@ import ButtonBanner from "../util/ButtonBanner"
 
 const UsSection = () => {
     return (
-        <div className="bg-[#3B3130]  flex flex-col md:flex-row items-center px-5 py-[60px] md:px-20">
-            <div className='md:w-1/2'>
-                <h1 className="text-4xl hidden md:block font-bold ">Conoce nuestra historia</h1>
+        <div className="bg-[#3B3130]  flex flex-col gap-5 lg:flex-row items-center px-5 py-[60px] lg:px-20">
+            <div className='lg:w-1/2'>
+                <h1 className="text-4xl text-center lg:text-left font-bold ">Conoce nuestra historia</h1>
                 <br />
-                <p className="text-lg font-semibold text-center md:text-left">Empresa líder del sector inmobiliario en el norte del país, tiene más de catorce años aportando al desarrollo urbanístico ordenado en la ciudad de Piura. Ofrecemos un concepto diferente en el cual valoramos la arborización y cuidamos que nuestros proyectos cuenten con grandes parques, espacios amplios y zonas de esparcimiento, contribuyendo a mejorar la calidad de vida de miles de familias.</p>
+                <p className="text-lg font-semibold text-left">Empresa líder del sector inmobiliario en el norte del país, tiene más de catorce años aportando al desarrollo urbanístico ordenado en la ciudad de Piura. Ofrecemos un concepto diferente en el cual valoramos la arborización y cuidamos que nuestros proyectos cuenten con grandes parques, espacios amplios y zonas de esparcimiento, contribuyendo a mejorar la calidad de vida de miles de familias.</p>
                 <br />
-                <p className="text-lg font-semibold text-center md:text-left">A la fecha se han desarrollado seis importantes proyectos contando ya con más de 6500 clientes que han cumplido el anhelado sueño de la casa propia en nuestras urbanizaciones..</p>
+                <p className="text-lg font-semibold text-left">A la fecha se han desarrollado seis importantes proyectos contando ya con más de 6500 clientes que han cumplido el anhelado sueño de la casa propia en nuestras urbanizaciones..</p>
                 <br />
-                <div className='flex justify-center md:justify-start'>
+                <div className='flex justify-center lg:justify-start'>
                     <ButtonBanner texto="Conoce más" />
                 </div>
             </div>
-            <div className='md:w-1/2'>
+            <div className='lg:w-1/2'>
                 <div className='flex flex-col  gap-5'>
-                    <div className='flex justify-center'>
+                    <div className='flex justify-end'>
                         <img className=" w-[480px] saturate-150" src="/img/valledoradonosotros.jpg" alt="GoldenEvent" />
                     </div>
                 </div>
